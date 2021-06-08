1e9fa05ed5870f3958984996302055747f9ebcc6
miliberlin <michelle.liebheit@posteo.de>
2021-06-08 19:13:30 +0200

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import projects from "./projects.json";
