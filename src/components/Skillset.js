047e3c9113bba5dc72935425f23501fbe0a9e799
miliberlin <michelle.liebheit@posteo.de>
2021-07-01 22:23:56 +0200

    added progress bars for skills

--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -5,13 +5,38 @@ export default function Skillset() {
     <div>
       <h2 className="title is-2">Skillset</h2>
       <ul>
-        <li>JavaScript ES6</li>
-        <li>React</li>
-        <li>MongoDB</li>
-        <li>Mongoose</li>
-        <li>Express.js</li>
-        <li>HTML</li>
-        <li>CSS</li>
+        <li>
+          JavaScript ES6
+          <progress class="progress is-primary" value="60" max="100" />
+        </li>
+        <li>
+          React
+          <progress class="progress is-primary" value="40" max="100" />
+        </li>
+        <li>
+          MongoDB
+          <progress class="progress is-primary" value="30" max="100" />
+        </li>
+        <li>
+          Express.js
+          <progress class="progress is-primary" value="30" max="100" />
+        </li>
+        <li>
+          HTML
+          <progress class="progress is-primary" value="90" max="100" />
+        </li>
+        <li>
+          CSS
+          <progress class="progress is-primary" value="90" max="100" />
+        </li>
+        <li>
+          Currently studying: TypeScript
+          <progress class="progress is-primary" max="100" />
+        </li>
+        <li>
+          Currently studying: Testing with Jest
+          <progress class="progress is-primary" max="100" />
+        </li>
       </ul>
     </div>
   );
